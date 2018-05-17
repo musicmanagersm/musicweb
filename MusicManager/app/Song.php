a3ca2d7a4c7a4bce94bca13403f58864425bfd40
jhon3724 <jhon3724@gmail.com>
2018-05-17 17:01:12 -0500

    modelo song

---
+++ MusicManager/app/Song.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MusicManager;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','artista','avatar'];
+
+}
